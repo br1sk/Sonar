066485eeb509e8f04b954abe5f5c313269ba9179
Keith Smiley <keithbsmiley@gmail.com>
2016-06-19 21:12:19 -0700

    Fix bug classifications

--- Sources/Sonar/Models/ModelsData.swift
+++ Sources/Sonar/Models/ModelsData.swift
@@ -4,13 +4,13 @@ extension Classification {
     public static let Power = Classification(appleIdentifier: 3, name: "Power")
     public static let Performance = Classification(appleIdentifier: 4, name: "Performance")
     public static let UI = Classification(appleIdentifier: 5, name: "UI/Usability")
-    public static let SeriousBug = Classification(appleIdentifier: 6, name: "Serious Bug")
+    public static let SeriousBug = Classification(appleIdentifier: 7, name: "Serious Bug")
     public static let OtherBug = Classification(appleIdentifier: 8, name: "Other Bug")
     public static let Feature = Classification(appleIdentifier: 10, name: "Feature (New)")
     public static let Enhancement = Classification(appleIdentifier: 11, name: "Enhancement")
 
     public static let All: [Classification] = [
-        .Security, .Crash, .Power, .Performance, .UI, .SeriousBug, .Feature, .Enhancement
+        .Security, .Crash, .Power, .Performance, .UI, .SeriousBug, .OtherBug, .Feature, .Enhancement
     ]
 }
 
