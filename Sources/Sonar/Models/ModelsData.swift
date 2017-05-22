0a959ef8ed3f15c15217e560b37186239700cf3a
Keith Smiley <k@keith.so>
2017-05-21 22:01:18 -0700

    Update iOS Areas for new data (#17)

--- Sources/Sonar/Models/ModelsData.swift
+++ Sources/Sonar/Models/ModelsData.swift
@@ -28,66 +28,42 @@ extension Reproducibility {
 }
 
 extension Area {
-    public static let Accessibility = Area(appleIdentifier: 1, name: "Accessibility")
-    public static let APNS = Area(appleIdentifier: 2, name: "APNS (Notifications)")
-    public static let AppSwitcher = Area(appleIdentifier: 3, name: "App Switcher")
-    public static let AVFoundation = Area(appleIdentifier: 4, name: "AVFoundation (Audio / Video)")
-    public static let BatteryLife = Area(appleIdentifier: 5, name: "Battery Life")
-    public static let Bluetooth = Area(appleIdentifier: 6, name: "Bluetooth")
-    public static let Calendar = Area(appleIdentifier: 7, name: "Calendar")
-    public static let CarPlay = Area(appleIdentifier: 8, name: "CarPlay")
-    public static let CellularService = Area(appleIdentifier: 9, name: "Cellular Service (Calls / Data)")
-    public static let CloudKit = Area(appleIdentifier: 10, name: "CloudKit (iCloud)")
-    public static let Contacts = Area(appleIdentifier: 11, name: "Contacts")
-    public static let ControlCenter = Area(appleIdentifier: 12, name: "Control Center")
-    public static let CoreLocation = Area(appleIdentifier: 13, name: "CoreLocation (Location Services)")
-    public static let DeviceManagement = Area(appleIdentifier: 14, name: "Device Management / Profiles")
-    public static let Facetime = Area(appleIdentifier: 15, name: "Facetime")
-    public static let GameKit = Area(appleIdentifier: 16, name: "GameKit")
-    public static let HealthKit = Area(appleIdentifier: 17, name: "HealthKit")
-    public static let HomeKit = Area(appleIdentifier: 18, name: "HomeKit")
-    public static let iPodAccessoryProtocol = Area(appleIdentifier: 19, name: "iPod Accessory Protocol (AP)")
-    public static let iTunesConnect = Area(appleIdentifier: 20, name: "iTunes Connect")
-    public static let iTunesStore = Area(appleIdentifier: 21, name: "iTunes Store")
-    public static let Keyboard = Area(appleIdentifier: 22, name: "Keyboard")
-    public static let LockScreen = Area(appleIdentifier: 23, name: "Lock Screen")
-    public static let Mail = Area(appleIdentifier: 24, name: "Mail")
-    public static let MapKit = Area(appleIdentifier: 25, name: "MapKit")
-    public static let Messages = Area(appleIdentifier: 26, name: "Messages")
-    public static let Metal = Area(appleIdentifier: 27, name: "Metal")
-    public static let Music = Area(appleIdentifier: 28, name: "Music")
-    public static let NightShift = Area(appleIdentifier: 29, name: "Night Shift")
-    public static let Notes = Area(appleIdentifier: 30, name: "Notes")
-    public static let NotificationCenter = Area(appleIdentifier: 31, name: "Notification Center")
-    public static let NSURL = Area(appleIdentifier: 32, name: "NSURL")
-    public static let PhoneApp = Area(appleIdentifier: 33, name: "Phone App")
-    public static let Photos = Area(appleIdentifier: 34, name: "Photos")
-    public static let Reminders = Area(appleIdentifier: 35, name: "Reminders")
-    public static let SafariServices = Area(appleIdentifier: 36, name: "Safari Services")
-    public static let SceneKit = Area(appleIdentifier: 37, name: "SceneKit")
-    public static let SetupAssistant = Area(appleIdentifier: 38, name: "Setup Assistant")
-    public static let SoftwareUpdate = Area(appleIdentifier: 39, name: "Software Update")
-    public static let Spotlight = Area(appleIdentifier: 40, name: "Spotlight")
-    public static let SpringBorad = Area(appleIdentifier: 41, name: "SpringBoard (Home Screen)")
-    public static let SpriteKit = Area(appleIdentifier: 42, name: "SpriteKit")
-    public static let StoreKit = Area(appleIdentifier: 43, name: "StoreKit")
-    public static let SystemSlow = Area(appleIdentifier: 44, name: "System Slow/Unresponsive")
-    public static let TouchID = Area(appleIdentifier: 45, name: "TouchID")
-    public static let UIKit = Area(appleIdentifier: 46, name: "UIKit")
-    public static let VPN = Area(appleIdentifier: 47, name: "VPN")
-    public static let WebKit = Area(appleIdentifier: 48, name: "WebKit")
-    public static let WiFi = Area(appleIdentifier: 49, name: "Wi-Fi")
-    public static let Xcode = Area(appleIdentifier: 50, name: "Xcode")
-    public static let OtherArea = Area(appleIdentifier: 51, name: "Other Area")
+    private init(_ appleIdentifier: Int, _ name: String) {
+        self.appleIdentifier = appleIdentifier
+        self.name = name
+    }
 
-    public static let All: [Area] = [
-        .Accessibility, .APNS, .AppSwitcher, .AVFoundation, .BatteryLife, .Bluetooth, .Calendar, .CarPlay,
-        .CellularService, .CloudKit, .Contacts, .ControlCenter, .CoreLocation, .DeviceManagement, .Facetime,
-        .GameKit, .HealthKit, .HomeKit, .iPodAccessoryProtocol, .iTunesConnect, .iTunesStore, .Keyboard,
-        .LockScreen, .Mail, .MapKit, .Messages, .Metal, .Music, .NightShift, .Notes, .NotificationCenter,
-        .NSURL, .PhoneApp, .Photos, .Reminders, .SafariServices, .SceneKit, .SetupAssistant, .SoftwareUpdate,
-        .Spotlight, .SpringBorad, .SpriteKit, .StoreKit, .SystemSlow, .TouchID, .UIKit, .VPN, .WebKit, .WiFi,
-        .Xcode, .OtherArea
+    public static let AlliOSAreas: [Area] = [
+        Area(1, "Accelerate Framework"), Area(2, "Accessibility"), Area(3, "Accounts Framework"),
+        Area(4, "App Store"), Area(5, "App Switcher"), Area(6, "Audio"), Area(7, "Audio Toolbox"),
+        Area(8, "AVFoundation"), Area(9, "AVKit"), Area(10, "Battery Life"), Area(11, "Bluetooth"),
+        Area(12, "Calendar"), Area(13, "CallKit"), Area(14, "CarPlay"),
+        Area(15, "Cellular Service (Calls / Data)"), Area(16, "CFNetwork Framework"), Area(17, "CloudKit"),
+        Area(18, "Contacts"), Area(19, "Contacts Framework"), Area(20, "Control Center"),
+        Area(21, "Core Animation"), Area(22, "Core Bluetooth"), Area(23, "Core Data"),
+        Area(24, "Core Foundation"), Area(25, "Core Graphics"), Area(26, "Core Image"),
+        Area(27, "Core Location"), Area(28, "Core Media"), Area(29, "Core Motion"),
+        Area(30, "Core Spotlight"), Area(31, "Core Telephony Framework"), Area(32, "Core Video"),
+        Area(33, "EventKit"), Area(34, "External Accessory Framework"), Area(35, "FaceTime"),
+        Area(36, "Foundation"), Area(37, "GameplayKit"), Area(38, "GLKit"), Area(39, "Health App"),
+        Area(40, "HealthKit"), Area(41, "Home App"), Area(42, "Home Screen"), Area(43, "HomeKit"),
+        Area(44, "iCloud"), Area(45, "Image I/O"), Area(46, "Intents Framework"), Area(47, "IOKit"),
+        Area(48, "iPod Accessory Protocol (iAP)"), Area(49, "iTunes Connect"), Area(50, "iTunes Store"),
+        Area(51, "Keyboard"), Area(52, "Local Authentication Framework"), Area(53, "Location Services"),
+        Area(54, "Lock Screen"), Area(55, "Mail"), Area(56, "MapKit"), Area(57, "Maps"), Area(58, "MDM"),
+        Area(59, "Media Player Framework"), Area(60, "Messages"), Area(61, "Messages Framework"),
+        Area(62, "Metal"), Area(63, "Model I/O"), Area(64, "Multipeer Connectivity Framework"),
+        Area(65, "Music"), Area(66, "Network Extensions Framework"), Area(67, "Notes"),
+        Area(68, "Notification Center"), Area(69, "NotificationCenter Framework"), Area(70, "Notifications"),
+        Area(71, "PassKit"), Area(72, "Phone App"), Area(73, "Photos"), Area(74, "Photos Framework"),
+        Area(75, "Profiles"), Area(76, "PushKit"), Area(77, "QuickLook Framework"), Area(78, "Reminders"),
+        Area(79, "ReplayKit"), Area(80, "Safari"), Area(81, "Safari Services"), Area(82, "SceneKit"),
+        Area(83, "Security Framework"), Area(84, "Setup Assistant"), Area(85, "Simulator"), Area(86, "Siri"),
+        Area(87, "Social Framework"), Area(88, "Software Update"), Area(89, "Spotlight"),
+        Area(90, "SpriteKit"), Area(91, "StoreKit"), Area(92, "System Slow/Unresponsive"),
+        Area(93, "SystemConfiguration Framework"), Area(94, "TouchID"), Area(95, "UIKit"),
+        Area(96, "UserNotifications Framework"), Area(97, "VPN"), Area(98, "Wallet"), Area(99, "WebKit"),
+        Area(100, "Wi-Fi"), Area(101, "Xcode"), Area(102, "Something not on this list"),
     ]
 }
 
