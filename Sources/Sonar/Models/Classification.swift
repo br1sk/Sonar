48b65e9b95bbcee1b36affc34dd878b276499187
Keith Smiley <k@keith.so>
2017-06-20 23:03:01 -0700

    Add Equatable conformances to Classification and Reproducibility (#26)

--- Sources/Sonar/Models/Classification.swift
+++ Sources/Sonar/Models/Classification.swift
@@ -1,7 +1,11 @@
-public struct Classification {
+public struct Classification: Equatable {
     /// Internal apple's identifier
     public let appleIdentifier: Int
 
     /// The name of the classification; useful to use as display name (e.g. 'UI/Usability').
     public let name: String
+
+    public static func == (lhs: Classification, rhs: Classification) -> Bool {
+        return lhs.appleIdentifier == rhs.appleIdentifier && lhs.name == rhs.name
+    }
 }
--- Sources/Sonar/Models/Reproducibility.swift
+++ Sources/Sonar/Models/Reproducibility.swift
@@ -1,7 +1,11 @@
-public struct Reproducibility {
+public struct Reproducibility: Equatable {
     /// Internal apple's identifier
     public let appleIdentifier: Int
 
     /// The name of the reproducibility; useful to use as display name (e.g. 'Sometimes').
     public let name: String
+
+    public static func == (lhs: Reproducibility, rhs: Reproducibility) -> Bool {
+        return lhs.appleIdentifier == rhs.appleIdentifier && lhs.name == rhs.name
+    }
 }
