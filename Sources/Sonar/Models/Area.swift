eccff834ccef886f450a33f8668f7649ca5f328e
Keith Smiley <keithbsmiley@gmail.com>
2018-04-27 17:40:31 -0700

    Add Area initializer

--- Sources/Sonar/Models/Area.swift
+++ Sources/Sonar/Models/Area.swift
@@ -5,6 +5,11 @@ public struct Area: Equatable {
     /// The name of the area; useful to use as display name (e.g. 'App Switcher').
     public let name: String
 
+    public init(appleIdentifier: Int, name: String) {
+        self.appleIdentifier = appleIdentifier
+        self.name = name
+    }
+
     public static func == (lhs: Area, rhs: Area) -> Bool {
         return lhs.appleIdentifier == rhs.appleIdentifier && lhs.name == rhs.name
     }
